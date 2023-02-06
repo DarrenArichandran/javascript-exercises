710d4068e3abaf392a1823dc039404799ec3a54e
Darren Arichandran <d.arichandran@gmail.com>
2023-02-06 20:43:53 +0000

    Attempt 2 at function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,12 +1,11 @@
 const repeatString = function() {
-    let num = repeatString[1]
-    let string = repeatString[0]
+    let num = repeatString[1];
+    let string = (repeatString[0]);
 
 
-for (let i = 0; i<= num; i++){
-  let stringNum =  string + ((num-1) * string);
+for (let i = 1; i<= num; i++){
+  let stringNum =  string + ((i-1) * string) 
   return stringNum
-
 }
 
 };
