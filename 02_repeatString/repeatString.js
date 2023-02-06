6762df5d4100c6a1390cd7f0bc99c27feb04536c
Darren Arichandran <d.arichandran@gmail.com>
2023-02-06 20:14:00 +0000

    1st attempt with written function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,13 @@
 const repeatString = function() {
+    let num = repeatString[1]
+    let string = repeatString[0]
+
+
+for (let i = 0; i<= num; i++){
+  let stringNum =  string + ((num-1) * string);
+  return stringNum
+
+}
 
 };
 
