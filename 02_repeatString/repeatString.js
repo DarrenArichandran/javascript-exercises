a3d075fdb402ff21b2e3c939fb0d598308a9afb6
Darren Arichandran <d.arichandran@gmail.com>
2023-02-07 19:44:19 +0000

    Amend if statement

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -2,7 +2,7 @@ const repeatString = function(string, number) {
     let stringNum = ""
 
 for (let i = 1; i<= number; i++){
-    if (i<0) {
+    if (number <0) {
         return "Error"
     } else {
   stringNum += string;
