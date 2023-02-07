de7707cbdde232ad4ef3f6140f37b2ca96a4995d
Darren Arichandran <d.arichandran@gmail.com>
2023-02-07 19:04:27 +0000

    Attempt 3 at function
    Simplified the approach with string+string as previous attempt was bringing up NaMN value when multiplying a string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,13 +1,13 @@
-const repeatString = function() {
-    let num = repeatString[1];
-    let string = (repeatString[0]);
+const repeatString = function(string, number) {
+    let num = number;
+    let word = string;
 
 
 for (let i = 1; i<= num; i++){
-  let stringNum =  string + ((i-1) * string) 
+  let stringNum =  word + word
   return stringNum
 }
-
+// ((i-1) * word)  is returning NaN, need to resolve this
 };
 
 // Do not edit below this line
