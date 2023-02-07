4b7fe2835a9e7c8e0d86b477764bb68e0f0c5569
Darren Arichandran <d.arichandran@gmail.com>
2023-02-07 19:36:02 +0000

    Add if statement within loop

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -2,7 +2,11 @@ const repeatString = function(string, number) {
     let stringNum = ""
 
 for (let i = 1; i<= number; i++){
+    if (i<0) {
+        return "Error"
+    } else {
   stringNum += string;
+    }
 }
 return stringNum
 // ((i-1) * word)  is returning NaN, need to resolve this
