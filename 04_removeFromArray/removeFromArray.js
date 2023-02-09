fdc0961eb78456a1a3e602ec5b2dbecda79046f5
Darren Arichandran <d.arichandran@gmail.com>
2023-02-09 08:33:14 +0000

    function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArr, number) {
+ let result = myArr.filter (checkNumber);
+
+ function checkNumber(figure) {
+     return figure != number;
+ }
+ return result
+}
+
+//need to remove "number" from "myArr"
+
+
 
 };
 
