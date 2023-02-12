f133e520a3169a39e47e35abd373f42f754a0d62
Darren Arichandran <d.arichandran@gmail.com>
2023-02-12 15:51:40 +0000

    g

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,26 +1,11 @@
 const removeFromArray = function(myArr, ...args) { 
 
-    function doesntMatch(value ,...args){
-        return (value !== args[0]) || (value !== args[1])
+    for (let i = 0; i <= myArr.length(); i++){
+const filtered = myArr.filter(x => myArr[i] !== args[0] || myArr[i] !== args[1] );
     }
+    return filtered
+}
 
-  const newList = myArr.filter(doesntMatch);
-  return newList;
-  
-
-  }
-//for (i=0; i<= array.length(); i++){
- //   if (array[i] !== Arguments){
-   //     newList.push(array[i])
-    //}
-
-    //return newList
-//let filteredArray = Array.filter( function(match) {
-//  return match != Arguments 
-
-//need to remove "number" from "myArr"
-
-//const newList = myArr.filter( x => myArr !== args[0] || myArr[i] !== args[2] );
 
 
 // Do not edit below this line
