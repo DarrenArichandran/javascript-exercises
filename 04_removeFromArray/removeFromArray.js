8bc2874974a6e4d2bc6fe02981b9109c1d8cb0c9
Darren Arichandran <d.arichandran@gmail.com>
2023-02-12 18:23:33 +0000

    r4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,7 @@
 const removeFromArray = function(myArr, ...args) {
 
 for (let i=0; i <= (args.length) ; i++) {
-const filtered = myArr.filter( value => !value.includes(args[i]));
+const filtered = myArr.filter( value => !myArr.includes(args[i]));
 return filtered
 }
 }
