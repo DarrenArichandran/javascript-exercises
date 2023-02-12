6daa87dbca15ba40d9ea2b62652049419219ed71
Darren Arichandran <d.arichandran@gmail.com>
2023-02-12 22:05:10 +0000

    f

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,20 +2,24 @@ const sumAll = function( a, b) {
 let answer = 0;
 
 
-if (a < b) {
+if (Math.sign(a) <= 0 || Math.sign(b) <= 0){
+    return "ERROR"
+} else if (Math.sign(a) == NaN|| Math.sign(b) == NaN) {
+    return "ERROR"
+} else if (a < b) {
     let x = a;
     while (x <= b) {
         answer += x;
         x++;
     }
-    return answer
+    return answer;
 } else {
     let x = b;
     while (x <= a) {
         answer += x;
         x++;
     }
-    return answer
+    return answer;
 }
 }
 // Do not edit below this line
