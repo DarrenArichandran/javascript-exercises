1184a16684587c7a56faa647b2c2e73b7a4f7dad
Darren Arichandran <d.arichandran@gmail.com>
2023-02-12 14:39:19 +0000

    b

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,18 +1,26 @@
-const removeFromArray = function(array, ...Arguments) { 
-const newList =[]
-    
-for (i=0; i<= array.length(); i++){
-    if (array[i] !== Arguments){
-        newList.push(array[i])
-    }
+const removeFromArray = function(myArr, ...args) { 
+ let newList =[];
+ let myArr =[""];
 
+    for (i=0; i<= myArr.length ; i++){
+        if (myArr[i] === args[0] || myArr[i] === args[1]){
+            newList.push(myArr[i]);
+        }
+    }
     return newList
+}
+//for (i=0; i<= array.length(); i++){
+ //   if (array[i] !== Arguments){
+   //     newList.push(array[i])
+    //}
+
+    //return newList
 //let filteredArray = Array.filter( function(match) {
 //  return match != Arguments 
 
 //need to remove "number" from "myArr"
-}
-}
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
