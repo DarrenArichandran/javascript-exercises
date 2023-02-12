9103014f1919e34d05a39583b75181ea6af32c45
Darren Arichandran <d.arichandran@gmail.com>
2023-02-12 15:59:24 +0000

    i

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,8 +2,8 @@ const removeFromArray = function(myArr, ...args) {
 
     for (let i = 0; i <= 3; i++){
 const filtered = myArr.filter( x => myArr[i] !== args[0] || myArr[i] !== args[1] );
-    }
     return filtered
+}
 }
 
 
