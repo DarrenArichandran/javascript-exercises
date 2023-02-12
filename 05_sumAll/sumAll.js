bc79c210bab3a851f8938f5e5d1cfeb150e1dba4
Darren Arichandran <d.arichandran@gmail.com>
2023-02-12 21:31:05 +0000

    d

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,13 +1,22 @@
 const sumAll = function( a, b) {
+let answer = 0;
 
+
+if (a < b) {
     let x = a;
-    let answer = 0;
-
     while (x <= b) {
         answer += x;
         x++;
     }
     return answer
+} else {
+    let x = b;
+    while (x <= a) {
+        answer += x;
+        x++;
+    }
+    return answer
+}
 }
 // Do not edit below this line
 module.exports = sumAll;
