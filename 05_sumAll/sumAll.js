d66dff401f17fa2fc1904c11bcd8dbd4a8a76ca3
Darren Arichandran <d.arichandran@gmail.com>
2023-02-12 22:26:12 +0000

    gh

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -5,7 +5,9 @@ if (Math.sign(a) <= 0 || Math.sign(b) <= 0){
     return "ERROR"
 } else if (Math.sign(a) == NaN|| Math.sign(b) == NaN) {
     return "ERROR"
-} else if (Number.isinteger(a || b) === 0) {
+} else if (Number.isInteger(a) === 0) {
+    return "ERROR"
+} else if (Number.isInteger(b) === 0) {
     return "ERROR"
 } else if (a < b) {
     let x = a;
