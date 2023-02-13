3dbc0da729b3861a460043f97b201469221927ff
Darren Arichandran <d.arichandran@gmail.com>
2023-02-13 22:00:36 +0000

    b

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,12 +1,15 @@
 const leapYears = function(x) {
 
-if ( x % 4 == 0 || (x) % 400 == 0 ){
-    return true;
+if ( x % 4 == 0) {
+    if (x % 100) {
+        return false
+    } else ( x % 400 == 0) && (x % 4 ==0) {
+        return true
+    }
 } else {
     return false
 }
-
-};
+}
 
 // Do not edit below this line
 module.exports = leapYears;
