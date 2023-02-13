e2a11b325abace1f9f8b940cf958866736d63a75
Darren Arichandran <d.arichandran@gmail.com>
2023-02-13 22:15:30 +0000

    e

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,10 +1,10 @@
 const leapYears = function(x) {
 
 if ( x % 4 === 0) {
-    if (x % 100 === 0) {
-        return false;
-    } else if ( x % 400 === 0) {
+    if (x % 400 === 0) {
         return true;
+    } else if ( x % 100 === 0) {
+        return false;
     } else {
         return true;
     }
