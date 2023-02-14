8fb97633e4b31e41a9c7dd76c0f0516716c2e7ad
Darren Arichandran <d.arichandran@gmail.com>
2023-02-14 19:26:18 +0000

    a

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(c) {
+
+  const tempC = (c -32) * (5/9) ;
+  return Math.round(tempC *10)/10;
+
+
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(f) {
+
+  const tempF = f * (9/5) + 32;
+  return Math.round(tempF *10)/10;
+
 };
 
 // Do not edit below this line
